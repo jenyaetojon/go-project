d282a61bf933ef224ff34b4bd6251a9360886ba4
jenyaetojon <163357799+jenyaetojon@users.noreply.github.com>
2024-04-22 13:21:31 +0300

    Create cmdpreupdate.bat
    q2q22 русь матушка

---
+++ cmdpreupdate.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Проверка и удаление папки, если она существует
+if exist "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" (
+    rd /s /q "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7"
+)
+
+:: Блокировка запуска процесса iikoFront.Net.exe
+:loop
+tasklist | find /i "iikoFront.Net.exe" >nul 2>&1
+if errorlevel 1 (
+    echo Процесс iikoFront.Net.exe не найден, продолжаем работу.
+) else (
+    echo Процесс iikoFront.Net.exe найден, ждем его завершения.
+    timeout /t 5
+    goto loop
+)
+
+:: Копирование папки
+xcopy "C:\ProgramData\iiko\sample-1" "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" /E /I
