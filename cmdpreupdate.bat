afba379d5bb9b8557060383ec0923f19783bd4fa
jenyaetojon <163357799+jenyaetojon@users.noreply.github.com>
2024-05-02 22:56:15 +0300

    Update cmdpreupdate.bat

--- cmdpreupdate.bat
+++ cmdpreupdate.bat
@@ -19,3 +19,11 @@ if exist "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartS
 
 :: Копирование папки
 xcopy "C:\ProgramData\iiko\Resto.Front.Api.SmartSupportV7" "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" /E /I
+
+:: Пауза перед запуском iikoFront.Net.exe
+timeout /t 5
+
+:: Запуск iikoFront.Net.exe
+start "" /B "C:\Program Files\iiko\iikoRMS\Front.Net\iikoFront.Net.exe"
+
+echo Update completed.
