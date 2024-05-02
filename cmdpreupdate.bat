e2b08892401ef3a3fd4e7f50f223e914a4be3fd7
jenyaetojon <163357799+jenyaetojon@users.noreply.github.com>
2024-05-02 23:44:03 +0300

    Update cmdpreupdate.bat
    final v2_start

--- cmdpreupdate.bat
+++ cmdpreupdate.bat
@@ -8,7 +8,7 @@ if errorlevel 1 (
     echo Процесс iikoFront.Net.exe не найден, продолжаем работу.
 ) else (
     echo Процесс iikoFront.Net.exe найден, ждем его завершения.
-    timeout /t 5
+    timeout /t 1
     goto loop
 )
 
@@ -18,10 +18,10 @@ if exist "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartS
 )
 
 :: Копирование папки
-xcopy "C:\ProgramData\iiko\Resto.Front.Api.SmartSupportV7\Resto.Front.Api.SmartSupportV7" "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" /E /I
+xcopy "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7\Resto.Front.Api.SmartSupportV7" "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" /E /I
 
 :: Пауза перед запуском iikoFront.Net.exe
-timeout /t 5
+timeout /t 1
 
 :: Запуск iikoFront.Net.exe
 start "" /B "C:\Program Files\iiko\iikoRMS\Front.Net\iikoFront.Net.exe"
