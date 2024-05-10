7b619f61b1220b320379279dbc45c9aa479ba4dc
jenyaetojon <163357799+jenyaetojon@users.noreply.github.com>
2024-05-10 13:55:51 +0300

    Update cmdpreupdate.bat
    fffffffffffffffffffffffffff

--- cmdpreupdate.bat
+++ cmdpreupdate.bat
@@ -19,20 +19,12 @@ if exist "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartS
     rd /s /q "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7"
 )
 
-
+timeout /t 1
 :: Копирование папки
 xcopy "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7\Resto.Front.Api.SmartSupportV7" "C:\Program Files\iiko\iikoRMS\Front.Net\Plugins\Resto.Front.Api.SmartSupportV7" /E /I
 timeout /t 2
 
-:: Проверка и удаление файла, если он существует
-if exist "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7.zip" (
-    del /f /q "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7.zip"
-)
-timeout /t 1
-:: Проверка и удаление папки, если она существует
-if exist "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7" (
-    rd /s /q "C:\ProgramData\iiko\SmartSupport\Resto.Front.Api.SmartSupportV7"
-)
+
 
 
 
